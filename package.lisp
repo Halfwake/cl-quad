be9f0f3ab902838b6671ba7b237fab84edbfec93
Drew Dudash <drewd8@vt.edu>
2015-12-21 02:47:16 -0500

    add license header to package.lisp

--- package.lisp
+++ package.lisp
@@ -1,4 +1,19 @@
-;;;; package.lisp
+;;     Copyright 2015 Andrew "Drew" Dudash
+;;
+;;     This file is part of cl-quad.
+;;
+;;     cl-quad is free software: you can redistribute it and/or modify
+;;     it under the terms of the GNU General Public License as published by
+;;     the Free Software Foundation, either version 3 of the License, or
+;;     (at your option) any later version.
+;;
+;;     cl-quad is distributed in the hope that it will be useful,
+;;     but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;     GNU General Public License for more details.
+;;
+;;     You should have received a copy of the GNU General Public License
+;;     along with cl-quad.  If not, see <http://www.gnu.org/licenses/>.
 
 (defpackage #:cl-quad
   (:use #:cl
